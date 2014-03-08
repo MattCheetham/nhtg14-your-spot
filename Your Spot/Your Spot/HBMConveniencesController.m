39c6ce780ba43c8cd8a949fceebf6141f5be972c
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-08 11:26:37 +0000

    Stubs out shared controller

--- Your Spot/Your Spot/HBMConveniencesController.m
+++ Your Spot/Your Spot/HBMConveniencesController.m
@@ -8,6 +8,35 @@
 
 #import "HBMConveniencesController.h"
 
+@interface HBMConveniencesController ()
+
+@property (nonatomic, strong) NSMutableArray *conveniences;
+
+@end
+
 @implementation HBMConveniencesController
+
+static HBMConveniencesController *sharedController = nil;
+
++ (HBMConveniencesController *)sharedController
+{
+    @synchronized(self) {
+        if (sharedController == nil) {
+            sharedController = [[self alloc] init];
+        }
+    }
+    return sharedController;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+        self.conveniences = [NSMutableArray array];
+        
+    }
+    return self;
+}
 
 @end
