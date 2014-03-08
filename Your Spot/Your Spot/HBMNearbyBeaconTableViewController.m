aa6042effe21aea1df68bc397076cf4492ebed2d
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-08 14:33:02 +0000

    Adds tableview to display beacon

---
+++ Your Spot/Your Spot/HBMNearbyBeaconTableViewController.m
@@ -0,0 +1,56 @@
+//
+//  HBMNearbyBeaconTableViewController.m
+//  Your Spot
+//
+//  Created by Matthew Cheetham on 08/03/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "HBMNearbyBeaconTableViewController.h"
+
+@interface HBMNearbyBeaconTableViewController ()
+
+@end
+
+@implementation HBMNearbyBeaconTableViewController
+
+- (id)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    
+    return cell;
+}
+@end
