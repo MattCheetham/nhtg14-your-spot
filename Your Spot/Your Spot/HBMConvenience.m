f371889e6bac8381fd4258873bcd8e1bd1a4d1ee
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-08 12:32:24 +0000

    Fixes incorrect dictionary keys for convenience latitude and logitude

--- Your Spot/Your Spot/HBMConvenience.m
+++ Your Spot/Your Spot/HBMConvenience.m
@@ -17,7 +17,7 @@
         
         self.convenienceName = dictionary[@"Sub_Building_Name"];
         self.convenienceAddress = dictionary[@"FULL_ADDRESS"];
-        self.convenienceLocation = [[CLLocation alloc] initWithLatitude:[dictionary[@"x"] doubleValue] longitude:[dictionary[@"y"] doubleValue]];
+        self.convenienceLocation = [[CLLocation alloc] initWithLatitude:[dictionary[@"X"] doubleValue] longitude:[dictionary[@"Y"] doubleValue]];
         
     }
     return self;
