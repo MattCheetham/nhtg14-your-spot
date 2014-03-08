fdd58b8dd47c44033f8e041ce0e32aa9e352dcc9
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-08 10:44:18 +0000

    Basic Xcode project set up

---
+++ Your Spot/Your Spot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Your Spot
+//
+//  Created by Matthew Cheetham on 08/03/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBMAppDelegate class]));
+    }
+}
---
+++ Your Spot/Your SpotTests/Your_SpotTests.m
@@ -0,0 +1,34 @@
+//
+//  Your_SpotTests.m
+//  Your SpotTests
+//
+//  Created by Matthew Cheetham on 08/03/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Your_SpotTests : XCTestCase
+
+@end
+
+@implementation Your_SpotTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
