c370244b52ae7c61ea55a8452c5310f57ddc50ce
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-08 12:52:22 +0000

    Adds controller

---
+++ Your Spot/Your Spot/HBMBeaconController.m
@@ -0,0 +1,13 @@
+//
+//  HBMBeaconController.m
+//  Your Spot
+//
+//  Created by Matthew Cheetham on 08/03/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "HBMBeaconController.h"
+
+@implementation HBMBeaconController
+
+@end
