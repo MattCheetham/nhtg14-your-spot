c043e61b0e17182a3b5473539c9bb90bf27b49c1
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-03-09 10:57:52 +0000

    Removes redundant child information

--- Your Spot/Your Spot/HBMChild.m
+++ Your Spot/Your Spot/HBMChild.m
@@ -14,9 +14,6 @@
 {
     self = [super init];
     if (self) {
-//        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"B0702880-A295-A8AB-F734-031A98A512DE"] identifier:@"child"];
-        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"B0702880-A295-A8AB-F734-031A98A512DE"] identifier:@"child"];
-//        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"] identifier:@"child"];
         self.beaconRegion.notifyEntryStateOnDisplay = YES;
         self.beaconRegion.notifyOnEntry = YES;
         self.beaconRegion.notifyOnExit = YES;
